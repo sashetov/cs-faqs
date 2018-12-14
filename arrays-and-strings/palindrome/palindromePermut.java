e2064ed575c82c9a3be5eb5d12a0e1c0b2f3ccf9
Alexander Vassilevski <alexander.vassilevski@gmail.com>
2018-12-14 15:26:22 -0800

    added bitshift palindromepermutation problem

--- arrays-and-strings/palindrome/palindromePermut.java
+++ arrays-and-strings/palindrome/palindromePermut.java
@@ -1,15 +1,5 @@
 import java.util.*;
 public class palindromePermut {
-  public static void main(String... args) {
-    String[] strs= { "sssseeh", "seshs es", "atco cta","taco cat", "mthing else" };
-    for ( int i = 0; i < strs.length; i++ ){
-      System.out.println( strs[i]+": "+ Boolean.toString(isPermutationOfPalindrome( strs[i])) );
-    }
-  }
-  static boolean isPermutationOfPalindrome( String phrase ) {
-    int[] table = buildCharFrequencyTable( phrase );
-    return checkMaxOneOdd( table );
-  }
   static boolean checkMaxOneOdd( int[] table ) {
     boolean foundOdd = false;
     for ( int count : table ) {
@@ -40,5 +30,60 @@ public class palindromePermut {
       }
     }
     return table;
+  }
+  static boolean isPermutationOfPalindrome( String phrase ) { // o(n) time, no optimizations ( two pass ), s space complexity for table
+    int[] table = buildCharFrequencyTable( phrase );
+    return checkMaxOneOdd( table );
+  }
+  static boolean isPermutationOfPalindromeOnePass( String phrase ) { // o(n) time, alternate ( single pass ), s space complexity for table
+    int countOdd = 0;
+    int a = Character.getNumericValue('a');
+    int z = Character.getNumericValue('z');
+    int[] table = new int[z-a+1];
+    for( char c : phrase.toCharArray()){
+      int x = getCharNumber(c);
+      if( x!=-1){
+        table[x]++;
+        if( table[x] %2 ==1 ){
+          countOdd++;
+        }
+        else countOdd--;
+      }
+    }
+    return countOdd <=1;
+  }
+  static int toggle( int bitVector, int index) {
+    if( index < 0 ) return bitVector;
+    int mask = 1<<index;
+    if( ( bitVector & mask )==0){
+      bitVector |=mask;
+    }
+    else{
+      bitVector &= ~mask;
+    }
+    return bitVector;
+  }
+  static int createBitVector( String phrase ){
+    int bitVector=0;
+    for (char c: phrase.toCharArray()){
+      int x = getCharNumber(c);
+      bitVector = toggle( bitVector, x );
+    }
+    return bitVector;
+  }
+  static boolean checkExactlyOneBitSet( int bitVector){
+    return ( bitVector & ( bitVector -1 )) == 0;
+  }
+  static boolean isPermutationOfPalindromeBitShift( String phrase ){ //o(n) time complexity, 1 space complexity for int
+    int bitVector = createBitVector( phrase );
+    return bitVector == 0 || checkExactlyOneBitSet( bitVector );
+  }
+  public static void main(String... args) {
+    String[] strs= { "sssseeh", "seshs es", "atco cta","taco cat", "mthing else" };
+    for ( int i = 0; i < strs.length; i++ ){
+      System.out.println( "isPermutationOfPalindrome " + strs[i]+": "+ Boolean.toString(isPermutationOfPalindrome( strs[i])) );
+      System.out.println( "isPermutationOfPalindromeOnePass " + strs[i]+": "+ Boolean.toString(isPermutationOfPalindromeOnePass( strs[i])) );
+      System.out.println( "isPermutationOfPalindromeBitShift" + strs[i]+": "+ Boolean.toString(isPermutationOfPalindromeBitShift( strs[i])) );
+    }
   }
 }
